72276d16bedff7db9e289cfd6cb0ac8b797231d6
Pedro <pedro2007.prrr@gmail.com>
2023-12-14 19:21:54 -0300

    I don't know what this commit does

--- src/main/java/net/kyrptonaught/lceui/mixin/container/HandledScreenMixin.java
+++ src/main/java/net/kyrptonaught/lceui/mixin/container/HandledScreenMixin.java
@@ -4,14 +4,21 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.kyrptonaught.lceui.LCEDrawableHelper;
 import net.kyrptonaught.lceui.LCEUIMod;
+import net.kyrptonaught.lceui.mixin.HandledScreenAccessor;
+import net.kyrptonaught.lceui.mixin.ScreenAccessor;
+import net.kyrptonaught.lceui.screens.LCEContainerScreen;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @SuppressWarnings("ConstantConditions")
 @Mixin(HandledScreen.class)
@@ -32,5 +39,15 @@ public class HandledScreenMixin {
             return (int) LCEDrawableHelper.drawText(instance, textRenderer, text, 9 + 1.0f/3.0f, this.playerInventoryTitleY, 2.0f/3.0f, 0x373737);
         }
         return original.call(instance, textRenderer, text, x, y, color, shadow);
+    }
+
+    @Inject(method = "init", at = @At("HEAD"), cancellable = true)
+    private void init(CallbackInfo ci) {
+        MinecraftClient client = ((ScreenAccessor) this).client();
+        if (client != null) {
+            if (((HandledScreen<?>) (Object) this) instanceof GenericContainerScreen && LCEUIMod.getConfig().containerInventory && !LCEUIMod.getConfig().compatibilityMode) {
+                client.setScreen(new LCEContainerScreen((GenericContainerScreenHandler) ((HandledScreenAccessor<?>) this).handler(), ((ScreenAccessor) this).client().player, ((ScreenAccessor) this).title()));
+            }
+        }
     }
 }
