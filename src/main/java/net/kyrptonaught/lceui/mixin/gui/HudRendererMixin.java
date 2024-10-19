6fafeb9ac612a243a1e0c281d1d1e6c654366782
S_PR0 <49575478+S-N00B-1@users.noreply.github.com>
2024-10-19 05:14:16 +0000

    Fix LEM Menu Compatibility

--- src/main/java/net/kyrptonaught/lceui/mixin/gui/HudRendererMixin.java
+++ src/main/java/net/kyrptonaught/lceui/mixin/gui/HudRendererMixin.java
@@ -4,9 +4,13 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.kyrptonaught.lceui.LCEUIMod;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.BossBarHud;
+import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.entity.Entity;
+
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,8 +19,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
-public abstract class HudRendererMixin {
+public class HudRendererMixin {
     @Shadow @Final private MinecraftClient client;
+    @Shadow private void renderVignetteOverlay(DrawContext context, Entity cameraEntity) {}
+    @Shadow @Final private BossBarHud bossBarHud;
+    @Shadow private int ticks;
+
+    @Shadow @Final private ChatHud chatHud;
 
     @Inject(method = "render", at=@At("HEAD"), cancellable = true)
     public void render(DrawContext context, float tickDelta, CallbackInfo ci) {
@@ -27,9 +36,19 @@ public abstract class HudRendererMixin {
             } else {
                 RenderSystem.enableDepthTest();
             }
+
+            // While yes chat and bossbars should *technically* be hidden, actually, lem menu.
+            this.client.getProfiler().push("bossHealth");
+            this.bossBarHud.render(context);
+
+            int n = MathHelper.floor(this.client.mouse.getX() * client.getWindow().getScaledWidth() / client.getWindow().getWidth());
+            int p = MathHelper.floor(this.client.mouse.getY() * client.getWindow().getScaledHeight() / client.getWindow().getHeight());
+            this.client.getProfiler().push("chat");
+            this.chatHud.render(context, this.ticks, n, p);
+            this.client.getProfiler().pop();
+
             ci.cancel();
         }
     }
 
-    @Shadow private void renderVignetteOverlay(DrawContext context, Entity cameraEntity) {};
 }
